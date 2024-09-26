fd6f26605ce5c6fb57af1100201f21710387d008
A$AP_Pretty <hdvillatoro@scl.edu.gt>
2024-09-26 10:04:41 -0600

    Segunda version de portafolio - Se agrega el navBar - Se agrega el header - Se agrega un elemento item de guia

--- componentes/proyectos/proyectos.js
+++ componentes/proyectos/proyectos.js
@@ -1,30 +1,12 @@
+import { header } from "../header/header.js";
+
 function proyectos(){
     let section = document.createElement('section');
     section.className = "section-header"
     section.appendChild(header())
     section.appendChild(lista())
 
-
-
 return section;
-}
-
-//funcion header
-
-function header(){
-    let div = document.createElement('div');
-    div.className = "header";
-
-    let h2 = document.createElement('h2');
-    h2.innerHTML = "Proyectos"
-    div.appendChild(h2);
-
-    let bttn = document.createElement('div');
-    bttn.innerHTML = "Github";
-    div.appendChild(bttn);
-
-
-return div;
 }
 
 //item lista
